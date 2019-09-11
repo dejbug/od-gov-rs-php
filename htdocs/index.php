f3a87b8f0ef5f7c59ae1023c20e0d0ca742f67ad
Dejan Budimir <dejbug@users.noreply.github.com>
2019-09-11 12:09:50 +0200

    fix: heights of marked rows were rendered smaller

--- htdocs/index.php
+++ htdocs/index.php
@@ -41,21 +41,6 @@ foreach ($json["data"] as $x) {
 ?>
 <p><small>(... and there's more where this came from.)</small></p>
 </div>
-<script>
-function cb_toggle_expand(node) {
-	// toggle_expand_with_last(node);
-	toggle_expand(node);
-}
-var last = null;
-function toggle_expand_with_last(node) {
-	if (last) toggle_expand(last);
-	if (last === node) last = null;
-	else { toggle_expand(node); last = node; }
-}
-function toggle_expand(node) {
-	node.firstChild.classList.toggle("long");
-	node.classList.toggle("row-expanded");
-}
-</script>
+<script src="index.js"></script>
 </body>
 </html>
