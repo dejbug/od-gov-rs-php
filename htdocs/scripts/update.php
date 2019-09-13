d7b5e6eaf55d44edd9b24bab44b0843591f3bc5d
Dejan Budimir <dejbug@users.noreply.github.com>
2019-09-13 13:46:30 +0200

    webhook test trigger 3

--- htdocs/scripts/update.php
+++ htdocs/scripts/update.php
@@ -3,13 +3,13 @@ $f = fopen('update.log', 'a');
 fwrite($f, date('r', $_SERVER['REQUEST_TIME'])."\r\n");
 fwrite($f, $_SERVER['REMOTE_ADDR'].' ('.$_SERVER['REMOTE_HOST'].")\r\n");
 fwrite($f, $_SERVER['HTTP_USER_AGENT']."\r\n");
-fwrite($f, $_SERVER['HTTP_REFERER']."\r\n");
+fwrite($f, var_export($_POST, TRUE)."\r\n");
 fwrite($f, "---\r\n");
 fclose($f);
 
 if (!array_key_exists('SECRET', $_POST)) die ('*');
 
-$secret = file_get_contents('update.secret');
+$secret = trim(file_get_contents('update.secret'));
 if (FALSE === $secret) die ('>');
 if (strlen($secret) < 16) die ('#');
 if ($_POST['SECRET'] != $secret) die ('=');
